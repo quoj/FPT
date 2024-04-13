dd78128d24c054bba1c3bcf54f749bf23a714986
quoj <133200203+quoj@users.noreply.github.com>
2024-04-13 10:29:20 +0700

    Update DatabaseSeeder.php

--- DatabaseSeeder.php
+++ DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        //     'name' => 'User',
+        //     'email' => 'user@example.com',
         // ]);
         Book::factory(50)->create();
     }
